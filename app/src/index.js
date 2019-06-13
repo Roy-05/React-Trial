5266996f1e34e930915a9284d70247e2316d1a98
Saket Roy <roy05@live.missouristate.edu>
2019-06-14 04:42:29 +0530

    Add background image and create div for Title

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import './css/Home.css';
 
-const App = () => (
-    <div>
-      <h1>Hello world!!</h1>
+
+const divStyle = {
+  position: 'absolute',
+  margin: 'auto',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  width: '200px',
+  height: '200px',
+};
+
+const h1Style  = {
+  color: 'white',
+  font: 'Verdana',
+  fontSize: '50px'
+}
+const Home = () => (
+    <div style = {divStyle}>
+      <h1 style = {h1Style}>Welcome</h1>
     </div>
     )
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Home/>, document.getElementById('root'));
