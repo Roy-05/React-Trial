b726e185eb17f840989077428c98915db5a76f78
Saket Roy <roy05@live.missouristate.edu>
2019-06-17 22:32:27 +0530

    add background image and load css from modules

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './css/Home.css';
+import './css/Home.css';
 
 
-const divStyle = {
+/*const divStyle = {
   position: 'absolute',
   margin: 'auto',
   top: 0,
   right: 0,
   bottom: 0,
   left: 0,
-  width: '200px',
-  height: '200px',
+  width: '500px',
+  height: '500px',
 };
 
 const h1Style  = {
-  color: 'white',
-  font: 'Verdana',
+  color: 'black',
+  fontFamily: 'Helvetica',
   fontSize: '50px'
-}
+}*/
+
+
 const Home = () => (
-    <div style = {divStyle}>
-      <h1 style = {h1Style}>Welcome</h1>
+    <div className = "center-div">
+      <h1 id = "title">I AM SAKET ROY</h1>
     </div>
     )
 ReactDOM.render(<Home/>, document.getElementById('root'));
