6069f376736833bc4f0b92c1de5b1e2abb2d8972
Saket Roy <roy05@live.missouristate.edu>
2019-06-23 00:46:52 +0530

    Move button code to individual component

---
+++ src/components/PrimaryButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PrimaryButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.redirect = this.redirect.bind(this);
+    }
+  
+    redirect(){
+      alert("Redirect to About me");
+    }
+  
+    render() {
+      return (
+        <span className = "button-container">
+          <button className = "primary-button" onClick={this.redirect}>{this.props.ButtonText}</button>
+        </span>
+      )
+    }
+  }
+
+  
+export default PrimaryButton;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PrimaryButton from './components/PrimaryButton'
 import "./css/main.css";
 //import '../node_modules/normalize.css/normalize.css';
 
@@ -12,6 +13,7 @@ const HomePage = () => {
         <Jumbotron />
       </div>
       <div className = "grid-item"></div>
+      <div className = "grid-item"></div>
     </div>
     </>
   )
@@ -21,28 +23,11 @@ class Jumbotron extends React.Component {
   render() {
     return (
       <div className = "jumbotron">
-        <h1 id = "title">I AM SAKET ROY</h1>
-        <h2 id = "fluff">STUDENT. DEVELOPER. TRAVELER</h2>
-        <PrimaryButton ButtonText = "EXPLORE"/>
-      </div>
-    )
-  }
-}
-
-class PrimaryButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.redirect = this.redirect.bind(this);
-  }
-
-  redirect(){
-    alert("Redirect to About me");
-  }
-
-  render() {
-    return (
-      <div className = "button-container">
-        <button id = "primary-button" onClick={this.redirect}>{this.props.ButtonText}</button>
+        <h1 id = "title">HI,I'M SAKET</h1>
+        <h2 id = "fluff">STUDENT. DEVELOPER. TRAVELER.</h2>
+        <div>
+          <PrimaryButton ButtonText = "LEARN MORE"/>
+        </div>
       </div>
     )
   }
