be220cf61faf1ba0306b0fc5e9a254c8d0751aea
Saket Roy <roy05@live.missouristate.edu>
2019-07-04 14:45:10 +0530

    set up gh-pages properly

---
+++ build/precache-manifest.af35c1894472e9411bdc21323c7e546a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d6747683b67447c91503555d458a3b",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "dfff3850a910f7f7f288",
+    "url": "/personal/static/css/main.d20b8c00.chunk.css"
+  },
+  {
+    "revision": "8775d5644869a40c7f99",
+    "url": "/personal/static/js/2.788a25dc.chunk.js"
+  },
+  {
+    "revision": "dfff3850a910f7f7f288",
+    "url": "/personal/static/js/main.12462c02.chunk.js"
+  },
+  {
+    "revision": "28e4a844449b63ec867d",
+    "url": "/personal/static/js/runtime~main.1ce33e60.js"
+  },
+  {
+    "revision": "90062a4bc849a4aa1a8d79638a4d928e",
+    "url": "/personal/static/media/background.90062a4b.jpg"
+  },
+  {
+    "revision": "ac5fe164d0b3c4ceef16ae87ae1967b1",
+    "url": "/personal/static/media/download.ac5fe164.png"
+  },
+  {
+    "revision": "98d7c8f6eaeebe1f5d59379a7636b779",
+    "url": "/personal/static/media/profile.98d7c8f6.jpg"
+  }
+]);
