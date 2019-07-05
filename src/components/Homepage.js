1193bf9832fbf68c2744f508e68d5a6efb25049a
Saket Roy <roy05@live.missouristate.edu>
2019-07-05 15:14:23 +0530

    Set up boilerplate code for react

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
-import Navbar from './Navbar';
-import "../css/homepage.css";
+import "../css/main.css"; 
 
 class HomePage extends React.Component {
   render(){
     return(
-      <div className = "grid-homepage">
-        <div className = "grid-item header">
-          <Navbar isHomeVisible = "hidden"/>
-        </div>
-        <div className = "grid-item"></div>
-        <div className = "grid-item jumbotron-container">
-          <div className = "jumbotron">
-            <h1 className = "title">HI,I'M SAKET</h1>
-            <h2 id = "fluff">STUDENT. DEVELOPER. TRAVELER.</h2>
-            <div>
-              <span className = "button-container">
-              <button className = "primary-button" onClick={() => this.props.history.push('about')}>
-                EXPLORE
-              </button>
-            </span>
-            </div>
-          </div>
-        </div>
-        <div className = "grid-item"></div>
+      <div className="homepage">
+        <h2>Get Started with React!</h2>
+        <p className="homepage-intro">
+          Start by adding react components in <code>src/components</code>.
+        </p>
       </div>
     )
   }
 }
 
-export default withRouter(HomePage);
+export default HomePage;
--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import HomePage from './Homepage';
-import Projects from './Projects';
-import Readings from './Readings';
-import Workbench from './Workbench';
-import AboutMe from './AboutMe';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const Routing = () => {
   return(
     <Router>
       <Route exact path = "/" component = {HomePage} />
-      <Route path = "/projects" component = {Projects} />
-      <Route path = "/readings" component = {Readings} />
-      <Route path = "/workbench" component = {Workbench} />
-      <Route path = "/about" component = {AboutMe} />
     </Router>
   ) 
 }
